07be728356d749ca0cbc50435b35efe7b49af905
Connor <gatorbearemail@gmail.com>
2020-04-21 15:23:00 -0400

    Adding android app to repo

--- canvas_to_web.py
+++ canvas_to_web.py
@@ -1,34 +1,23 @@
-from flask import Flask, render_template, request, redirect
-
-
-
 #Written By Connor Coddington
 #Goal of this file is to get information from Canvas to display on startpage
+
+
+from flask import Flask, render_template, request, redirect
 
 # Importing canvas class
 from canvasapi import canvas, Canvas, course, account, __init__, user
 import datetime
 
 
-#time = int(datetime.now())
+#This section gets the cerrent date then puts it in the format YYYYMMDD
+#We will use this to find assignments that are not due yet
 time = datetime.datetime.now()
 time = int(time.strftime('%Y%m%d'))
 print("Current date:")
 print(time)
 
-
-# time = datetime.datetime.now()
-# time.str()
-# currentyear, currentmonth, currentday = time.split('-')
-# print(currentyear,currentmonth,currentday)
-# print(time)
-
-#timeint = 10000*datetime.year + 100*datetime.month + datetime.day
-# timeint = int(time)
-# print(timeint)
-
-print("I hope you can see this")
-
+#This is the APIKEY for Connor Coddington. It will be deleted when the course ends because it is hosted on a public repository
+#For real implementation IT at Florida Poly could get a key that can read all users info
 #2607~xmwF872jdubyQweLR2MW56dYcFGKxIGNUnpISSBVfbauXsT8SxIidiTWs4rTMNO3
 
 #Florida Poly canvas URL
@@ -40,36 +29,36 @@ API_KEY = "2607~xmwF872jdubyQweLR2MW56dYcFGKxIGNUnpISSBVfbauXsT8SxIidiTWs4rTMNO3
 #Create Canvas object
 canvas = Canvas(API_URL, API_KEY)
 
-
-
-#account = canvas.get_account(4613)
-#user = canvas.get_user(4613)
-#logins = user.get_user_logins()
-
-#for login in logins:
-#    print(login)
-
+#Get all courses user is currently enrolled in
 courses = canvas.get_courses()
 
-todoCanvas = {'Assignment' : 'Course'}
-#todoCanvas
+
+#Creating lists to hold assignments, courses, and due dates
 counter = 0
 todoAssignments = []
 todoCourses = []
 todoDuedates = []
 
+
+#This loop is the meat of the file
+#It will return a list of assignments, courses, and due dates for assignments that are not due yet
 for Course in courses:
     try:
-        print(Course)
+        #print(Course)
         #classes = canvas.get_course(Course)
+        #Gets course info for every enrolled course
         classes = canvas.get_course(Course)
 
+        #Gets Every assignment from course
         assignments = classes.get_assignments()
 
+        #This for loop goes through every assignment
         for assignment in assignments:
             #print(assignment)
             #print(assignment.due_at)
+            #Removes assignments that do not have a due date
             if (assignment.due_at != None):
+                #This section takes the due date and converts it to YYYYMMDD format
                 year, month, continueSplit = assignment.due_at.split('-')
                 day, unused = continueSplit.split('T')
 
@@ -80,26 +69,34 @@ for Course in courses:
                 #print(yearint, monthint, dayint)
                 dueDate = (10000*yearint) + (100*monthint) + dayint
                 #print(dueDate)
+
+                #Enters this if the assignment is not due yet
                 if (dueDate >= time):
                     #print(assignment)
-                    #print("Im NOT DUE YET AAAAHAHHAHAHH")
+                    #Converts info to strings
                     strcourse = str(Course.name)
                     strassignment = str(assignment.name)
                     strduedate = str(assignment.due_at)
+
+                    #Adds all info lists
                     todoCourses.append(strcourse)
                     todoAssignments.append(strassignment)
                     todoDuedates.append(strduedate)
                     counter = counter + 1
+    #Courses from previously enrolled classes go to except
     except:
         pass
 
+#Prints lists to console
 print(todoDuedates)
 print(todoAssignments)
 todoCourses[3] = 'Software Engineering(SP 2020_CEN4010.01 ENGR) '
 print(todoCourses)
 
+print("Number of assignments:")
 print(len(todoDuedates))
 
+#Nice view of assignments that need to be done in console
 for x in range(len(todoDuedates)):
     print("----------New Assignment--------")
     print(todoCourses[x])
@@ -107,16 +104,19 @@ for x in range(len(todoDuedates)):
     print(todoDuedates[x])
 
 
-
+#Creates dictionary that will be placed in the website
 dictOfAssignment = {todoCourses[i] : todoAssignments[i] for i in range(0, len(todoAssignments)) }
 
 
+#This section interacts with the html files
 app = Flask(__name__)
 
+#Returns the assignment info to the website
 @app.route('/')
 def result():
    return render_template('index.html', result = dictOfAssignment)
 
+#Redirect for credits page
 @app.route('/credits', methods = ['GET', 'POST'])
 def credits():
     if request.method == 'POST':
@@ -125,6 +125,8 @@ def credits():
 
     return render_template('credits.html')
 
+
+#Hosting information
 if __name__ == '__main__':
    app.run(host="0.0.0.0")
   # app.run(debug = True)
